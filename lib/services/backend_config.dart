4c9831dc9dc4d3ee51c0f4cd21a686af078244de
nethra-108 <pptyisbad@gmail.com>
2026-09-16 22:31:02 +0530

    chore(config): hardcode physical device network IP for SIH presentation

--- lib/services/backend_config.dart
+++ lib/services/backend_config.dart
@@ -5,8 +5,8 @@ class BackendConfig {
 
   static String? _customBaseUrl;
 
-  static const String localAdbUrl = 'http://127.0.0.1:8000';
-  static const String androidEmulatorUrl = 'http://10.0.2.2:8000';
+  static const String localAdbUrl = 'http://192.168.0.110:8000';
+  static const String androidEmulatorUrl = 'http://192.168.0.110:8000';
   static const String productionHttpsUrl = 'https://api.shilpi.org';
 
   static String get baseUrl {
